06016ee775aea36cdfd61aa43ec33cf779af1503
Frank Wissink <frank.wissink@btc-echo.de>
2022-11-01 18:19:34 +0100

    change asset paths again

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: true,
   poweredByHeader: false,
+  assetPrefix: '/reviews',
+  rewrites() {
+    return [
+      {
+        source: '/reviews/_next/:path=*',
+        destination: '/_next/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
