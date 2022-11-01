b4e23b151fc66ff663f6858c1eae0f732483523c
Frank Wissink <frank.wissink@btc-echo.de>
2022-11-01 17:52:19 +0100

    change config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  trailingSlash: true,
+  poweredByHeader: false,
+  assetPrefix: isProd ? 'https://gcp.fwnx.de' : undefined,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
