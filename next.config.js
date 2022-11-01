1d3deb44daa5b89ae11ef0b1d639986d5ee83372
Frank Wissink <frank.wissink@btc-echo.de>
2022-11-01 17:58:09 +0100

    remove assetPath

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
   poweredByHeader: false,
-  assetPrefix: isProd ? 'https://gcp.fwnx.de' : undefined,
 };
 
 module.exports = nextConfig;
